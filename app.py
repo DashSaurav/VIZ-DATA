b5b37ab21169901832844fb2213c917b1c0ba823
Saurav Dash <102579410+DashSaurav@users.noreply.github.com>
2022-03-30 10:28:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,24 +6,16 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from pages import utils
 
-
+#st.set_page_config(layout = "wide")
 # custom imports
+
 from multipage import MultiPage
 from pages import data_upload, bar, line, scatter
 
 #creating app instance for multipage
 app = MultiPage()
 
-#st.set_page_config(layout = "wide")
-
-#logo.... if not present in same directory,, comment out region.
-header= st.container()
-with header:
-    padding = 0
-    st.markdown(f""" <style>reportview-container .main .block-container{{padding-top: {padding}rem;padding-right: {padding}rem;padding-left: {padding}rem;padding-bottom: {padding}rem;}} </style> """, unsafe_allow_html=True)
-    img = Image.open("MicrosoftTeams-image.png")
-    st.image(img, width=650)
-    st.title("Data Analytics")
+st.title("Data Analytics")
 
 
 # adding page application here, with crrct names.
