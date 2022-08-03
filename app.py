dcbdbe604024e138f80c14ef51b8d2d6a050b1b1
Saurav Dash <102579410+DashSaurav@users.noreply.github.com>
2022-08-03 18:13:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 from pages import utils
 
 #st.set_page_config(layout = "wide")
