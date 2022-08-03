7b113ffe06cd661f3e8ca2376e89562c99148f77
Saurav Dash <102579410+DashSaurav@users.noreply.github.com>
2022-08-03 18:25:30 +0530

    Update line.py

--- pages/line.py
+++ pages/line.py
@@ -11,19 +11,17 @@ def app():
     if 'main_data.csv' not in os.listdir('data'):
         st.markdown("Please upload data through `Upload Data` page!")
     else:
-        # df_analysis = pd.read_csv('data/2015.csv')
+        
         df_analysis = pd.read_csv('data/main_data.csv')
-        # df_visual = pd.DataFrame(df_analysis)
-        df_visual = df_analysis.copy()
-        cols = pd.read_csv('data/metadata/column_type_desc.csv')
-        Categorical,Numerical,Object = utils.getColumnTypes(cols)
-        cat_groups = {}
-        unique_Category_val={}
+        
 
         #code for line graph
         st.subheader('Showing Line Graph')
-        line_value = st.sidebar.selectbox('Select an Attribute for X-axis in Line Graph',df_analysis.columns)
-        line_value1 = st.sidebar.selectbox('Select an Attribute for Y-axis in Line Graph',df_analysis.columns)
+        opt = list(df_analysis.columns)
+        line_value = st.sidebar.selectbox('Select an Attribute for X-axis in Line Graph',opt)
+        opt = list(df_analysis.columns)
+        opt.remove(bar_value)
+        line_value1 = st.sidebar.selectbox('Select an Attribute for Y-axis in Line Graph',opt)
         # Add some matplotlib code !
         chart = alt.Chart(df_analysis).mark_line().encode(
             x=alt.X(line_value),
